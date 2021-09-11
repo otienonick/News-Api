c287eebde4bf18ec84d4d64f12ba413a9613a25f
otienonick <nick.odongi@student.moringaschool.com>
2021-09-11 11:00:19 +0300

    Created Everything class and instanciated variables to be initialized

---
+++ app/news.py
@@ -0,0 +1,13 @@
+class Everything:
+
+    '''
+    Everthing class to define Everything Objects
+
+    '''       
+    def __init__(self,title,description,url,urlToImage,content):
+        
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.content = content
