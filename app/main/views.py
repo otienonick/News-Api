8e4e75f76d5981dcaee9ce723cc056296cabdec1
otienonick <nick.odongi@student.moringaschool.com>
2021-09-11 23:24:01 +0300

    added the search function

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for
 from . import main
-from ..request import get_sources,get_articles,get_everything
+from ..request import get_sources,get_articles,get_everything,search_news
 
 # # Views
 
@@ -18,9 +18,11 @@ def index():
     title = 'Home - Welcome to Know It all'
     apple = get_sources()
     every = get_everything()
-
-
-    return render_template('index.html' , title = title  , apple =apple ,every = every)
+    search_all_news = request.args.get('news_query')
+    if search_all_news:
+        return redirect(url_for('main.search',search_news_name = search_all_news))
+    else:
+        return render_template('index.html' , title = title  , apple =apple ,every = every)
 
 
 
@@ -39,6 +41,28 @@ def news(name):
     return render_template('source.html',news=news)
 
 
+
+
+@main.route('/search/<search_news_name>')
+
+def search(search_news_name):
+
+    '''
+
+    View function to display the search results
+
+    '''
+
+    news_name_list = search_news_name.split(" ")
+
+    news_format = "+".join(news_name_list)
+
+    searched_news = search_news(news_format)
+
+    title = f'search results for {search_news_name}'
+
+
+    return render_template('search.html',title = title,news = searched_news)
 
 
 
