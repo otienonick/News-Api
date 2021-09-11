12e64724541810c0e6a0fda5958c356ea2cf9294
otienonick <nick.odongi@student.moringaschool.com>
2021-09-11 10:19:36 +0300

    created news api url and alsO initialized config options instance

---
+++ config.py
@@ -0,0 +1,39 @@
+import os
+class Config:
+
+    '''
+    General configuration parent class
+    
+    '''
+    
+    
+    NEWS_EVERYTHING_URL = 'https://newsapi.org/v2/everything?q=news&apiKey={}'
+
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('ead4ba544d5d4f75942062f71d3b4720')
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
