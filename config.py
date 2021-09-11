9778e1b4e2179220ab40c71aa85b2a02406b516e
otienonick <nick.odongi@student.moringaschool.com>
2021-09-11 23:25:00 +0300

    modified search section

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ class Config:
     
     NEWS_EVERYTHING_URL = 'https://newsapi.org/v2/everything?q=news&apiKey={}'
     NEWS_SOURCE_URL = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'
+    NEWS_ARTICLE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
 
 
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
