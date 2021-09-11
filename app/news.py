12674041b69440b447273132ee33d7c2b097bcce
otienonick <nick.odongi@student.moringaschool.com>
2021-09-11 18:43:03 +0300

    added news articles and news sources classes

--- app/news.py
+++ app/news.py
@@ -1,13 +1,42 @@
+class News_Source:
+    
+    '''
+    News class to define News Articles Objects
+
+    '''
+
+    def __init__(self,id,name,description,url,category):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+    
+
+class News_Articles:
+    
+    '''
+    News class to define News source Objects
+
+    '''
+
+    def __init__(self,source,author,title,description,url,urlToImage):
+        self.source = source
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+            
 class Everything:
 
     '''
     Everthing class to define Everything Objects
 
     '''       
-    def __init__(self,title,description,url,urlToImage,content):
+    def __init__(self,title,description,url,urlToImage):
         
         self.title = title
         self.description = description
         self.url = url
         self.urlToImage = urlToImage
-        self.content = content
