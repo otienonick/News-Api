0bdd7d2d08b91bcd107bb6da6d87c6dc14199f9e
otienonick <nick.odongi@student.moringaschool.com>
2021-09-11 20:44:45 +0300

    modified content class

--- app/news.py
+++ app/news.py
@@ -34,9 +34,10 @@ class Everything:
     Everthing class to define Everything Objects
 
     '''       
-    def __init__(self,title,description,url,urlToImage):
+    def __init__(self,title,description,url,urlToImage,content):
         
         self.title = title
         self.description = description
         self.url = url
         self.urlToImage = urlToImage
+        self.content = content
